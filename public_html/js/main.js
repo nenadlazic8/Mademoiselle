901de5a6393cfccb3c8a9122d9d2c7106b2b4f2e
nenadlazic8 <nenadolazico@hotmail.com>
2020-03-14 10:17:53 +0100

    adding costume animation

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    //CHECK HEADER
+
     function heroSectionMargin() {
         let headerH = $('header').innerHeight();
         $('.hero').css('margin-top', headerH);
@@ -11,12 +13,36 @@ $(document).ready(function () {
         heroSectionMargin();
     })
 
+    //OWL CAROUSEL
+
     $('.owl-carousel').owlCarousel({
         items : 1,
         loop : true
     });
 
-    AOS.init();
+    //ANIMATION ON SCROLL
+
+    //AOS.init();
+
+    //COSTUME ANIMATIONS
+    
+    function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animation = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+            if (position < scroll + windowHeight - 100) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animation);
+            }
+        });
+    }
+    animation();
+    $(window).scroll(function () {
+        animation();
+    });
 
 });
 
